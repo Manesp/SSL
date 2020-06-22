023b482439385868ea15f0ff88c6249166b42d7a
Manesp <mespina98@gmail.com>
2020-06-22 19:12:46 -0300

    Se agrega descripción de la funciones

--- 04-OperacionesStrings/string.h
+++ 04-OperacionesStrings/string.h
@@ -8,10 +8,17 @@ Manuel Ignacio Espina
 #include <stdio.h>
 #include <stdbool.h>
 
+/* La función cuenta la cantidad de caracteres de la cadena que recibe. */
 size_t GetLength(const char * str);
 
+/* La función valida si la cadena que recibe es vacía. Esto quiere decir que, 
+retornará un valor booleano dependiendo el valor de la cadena que ingrese. */
 bool IsEmpty(const char * str);
 
+/* La función toma una cadena y la multiplica por si misma las veces que el 
+usuario lo indique. */
 char * Power(const char * string, unsigned int power);
 
+/* La función valida si dos cadenas ingresadas son idénticas. Retornará 
+true, si son iguales, o false, si son diferentes. */
 bool AreEqual(const char * str1, const char * str2);
